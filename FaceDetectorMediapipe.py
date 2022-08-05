8bdd1071e66cce734b45a7f77c79b7ec01b3801e
agilulfe <l.nicaud@eleves-alsacienne.org>
2022-08-05 08:36:29 +0200

    2 corners better than 4 on second thoughts

--- FaceDetectorMediapipe.py
+++ FaceDetectorMediapipe.py
@@ -58,12 +58,6 @@ class FaceDetector():
         # Top Left  x,y
         cv2.line(img, (x, y), (x + l, y), self.bbox_color, t)
         cv2.line(img, (x, y), (x, y+l), self.bbox_color, t)
-        # Top Right  x1,y
-        #cv2.line(img, (x1, y), (x1 - l, y), self.bbox_color, t)
-        #cv2.line(img, (x1, y), (x1, y+l), self.bbox_color, t)
-        # Bottom Left  x,y1
-        #cv2.line(img, (x, y1), (x + l, y1), self.bbox_color, t)
-        #cv2.line(img, (x, y1), (x, y1 - l), self.bbox_color, t)
         # Bottom Right  x1,y1
         cv2.line(img, (x1, y1), (x1 - l, y1), self.bbox_color, t)
         cv2.line(img, (x1, y1), (x1, y1 - l), self.bbox_color, t)
