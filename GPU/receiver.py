0a51f3da52b95c5c71fc8bd022f3ec87c75416b9
agilulfe <l.nicaud@eleves-alsacienne.org>
2022-08-14 17:51:34 +0200

    adding low level UDP streaming

--- GPU/receiver.py
+++ GPU/receiver.py
@@ -1,17 +1,47 @@
+#!/usr/bin/env python
+
+from __future__ import division
 import cv2
-import sys
+import numpy as np
+import socket
+import struct
 
-# Read video
-video = cv2.VideoCapture("udpsrc port=5000 ! application/x-rtp,payload=96,encoding-name=H264 ! rtpjitterbuffer mode=1 ! rtph264depay ! h264parse ! decodebin ! videoconvert ! appsink", cv2.CAP_GSTREAMER)
+MAX_DGRAM = 2**16
 
-#video.set(cv2.CAP_PROP_BUFFERSIZE,3)
-# Exit if video not opened.
-if not video.isOpened():
-    print("Could not open video")
-    sys.exit()
+def dump_buffer(s):
+    """ Emptying buffer frame """
+    while True:
+        seg, addr = s.recvfrom(MAX_DGRAM)
+        print(seg[0])
+        if struct.unpack("B", seg[0:1])[0] == 1:
+            print("finish emptying buffer")
+            break
 
-# Read first frame.
-ok, frame = video.read()
-if not ok:
-    print('Cannot read video file')
-    sys.exit()
+def main():
+    """ Getting image udp frame &
+    concate before decode and output image """
+    
+    # Set up socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind(('127.0.0.1', 12345))
+    dat = b''
+    dump_buffer(s)
+
+    while True:
+        seg, addr = s.recvfrom(MAX_DGRAM)
+        if struct.unpack("B", seg[0:1])[0] > 1:
+            dat += seg[1:]
+        else:
+            dat += seg[1:]
+            img = cv2.imdecode(np.frombuffer(dat, dtype=np.uint8), 1)
+            cv2.imshow('frame', img)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+            dat = b''
+
+    # cap.release()
+    cv2.destroyAllWindows()
+    s.close()
+
+if __name__ == "__main__":
+    main()
