ffe0fb87d8e151ab9747d9f697c19ae90a4be837
agilulfe <l.nicaud@eleves-alsacienne.org>
2022-08-18 15:40:33 +0200

    creating __init__.py files

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import FaceDetectorMediapipe
-import FaceDetectorBlob
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+
+import FaceDetection.FaceDetectorMediapipe as FaceDetectorMediapipe
+import FaceDetection.FaceDetectorBlob as FaceDetectorBlob
 
 
 FaceDetectorMediapipe.main()    #faster
