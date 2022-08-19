85875c51cd854687879022f3da7922180ed5d143
agilulfe <l.nicaud@eleves-alsacienne.org>
2022-08-19 15:52:59 +0200

    detector

--- GPU/Detector.py
+++ GPU/Detector.py
@@ -23,7 +23,7 @@ class Detector:
 
         #I/O pipelines
         #self.camSet = 0
-        self.camSet='nvarguscamerasrc ! video/x-raw(memory:NVMM), width=3280, height=2464, format=(string)NV12, framerate=(fraction)15/1 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink'
+        self.camSet='nvarguscamerasrc ! video/x-raw(memory:NVMM), width=1280, height=720, format=(string)NV12, framerate=(fraction)15/1 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink'
         self.directToClient = cv2.VideoWriter('appsrc ! videoconvert ! x264enc tune=zerolatency bitrate=500 speed-preset=superfast ! rtph264pay ! udpsink host='+self.CLIENT_IP+' port='+str(self.CLIENT_PORT+1),cv2.CAP_GSTREAMER,0, 20, (dispW, dispH), True)
         self.out = cv2.VideoWriter('appsrc ! videoconvert ! x264enc tune=zerolatency bitrate=500 speed-preset=superfast ! rtph264pay ! udpsink host='+self.CLIENT_IP+' port='+str(self.CLIENT_PORT),cv2.CAP_GSTREAMER,0, 20, (dispW, dispH), True)
 
